e572ed8618aa6da22bb16f4f3518393086558267
Vazer Rahmani <vaz3r@outlook.com>
2018-10-20 03:03:54 +0100

    v1.1

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,33 @@
 from flask import Flask
+import requests
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
+
+@app.route('/version')
+def version():
+    return 'v1.0'
+
+@app.route('/scoreboard')
+def scoreboard():
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+            'Origin': 'stats.nba.com',
+            'Host': 'stats.nba.com',
+            'Referer': 'https://stats.nba.com/scores/10/19/2018',
+            'x-nba-stats-origin': 'stats',
+            'X-NewRelic-ID': 'VQECWF5UChAHUlNTBwgBVw==',
+            'x-nba-stats-token': 'true',
+        }
+
+        r = requests.get('https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate=10%2F19%2F2018', headers=headers)
+        return r.text
+    except:
+        return "Exception."
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
